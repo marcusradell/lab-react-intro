18a79e3b6e18f4b2d1a85fc346d1803d4656bbf2
Marcus Rådell <marcus@radell.net>
2024-07-11 10:32:37 +0200

    setup QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 
+const Data = () => {
+  return "no data";
+};
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>Lab React Intro</h1>
+      <Data />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
